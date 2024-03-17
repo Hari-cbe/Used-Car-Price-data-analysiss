dbc69fde916b1bf1a382ed4d4d0fe5f277dd23be
Hari-cbe <harisachin1025@gmail.com>
2024-03-17 11:06:37 +0000

    Configured the loc dir - container dir

--- dags/sourcetobucket.py
+++ dags/sourcetobucket.py
@@ -2,7 +2,6 @@ from airflow.decorators import dag, task
 import pendulum
 import os
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
-# from airflow.contrib.operators.file_to_gcs import FileToGoogleCloudStorageOperator
 
 @dag(
     dag_id = "source_to_gcs",
@@ -11,15 +10,19 @@ from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesyste
     schedule_interval='@daily',
     catchup=False
 )
+
+
 def source_to_gcs():
 
     upload_file = LocalFilesystemToGCSOperator(
         task_id="upload_file_gcs",
-        src='data/hello.txt', ## Change
-        dst='data/raw/compressed/raw/hello.txt', 
+        src='data/car_sales.csv',
+        dst='data/raw/car_sales.csv', 
         bucket='used-car-price-analysis',
         gcp_conn_id='gcp_airflow_conn'
     )
+
+    
 
     task1 = upload_file
 
