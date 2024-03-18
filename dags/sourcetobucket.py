50585f08b7d99652af66b3792c7b93ab4a2526a2
Hari-cbe <harisachin1025@gmail.com>
2024-03-18 02:11:32 +0000

    +created a sensor to check file

--- dags/sourcetobucket.py
+++ dags/sourcetobucket.py
@@ -2,6 +2,11 @@ from airflow.decorators import dag, task
 import pendulum
 import os
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
+
+
+BUCKET = 'used-car-price-analysis'
+GCP_CONN_ID = 'gcp_airflow_conn'
 
 @dag(
     dag_id = "source_to_gcs",
@@ -10,20 +15,34 @@ from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesyste
     schedule_interval='@daily',
     catchup=False
 )
-
-
 def source_to_gcs():
 
     upload_file = LocalFilesystemToGCSOperator(
         task_id="upload_file_gcs",
         src='data/car_sales.csv',
         dst='data/raw/car_sales.csv', 
-        bucket='used-car-price-analysis',
-        gcp_conn_id='gcp_airflow_conn'
+        bucket=BUCKET,
+        gcp_conn_id=GCP_CONN_ID
     )
 
-    
+    check_file = GCSObjectExistenceSensor(
+        task_id = "check_file",
+        bucket = BUCKET,
+        object = 'data/raw/car_sales.csv',
+        mode='poke',
+        timeout = 60 * 3,
+        google_cloud_conn_id = GCP_CONN_ID
+    )
 
-    task1 = upload_file
+    @task()
+    def confirm_file():
+        return "The File is present"
+
+    
+    task1 = upload_file 
+    task2 = check_file
+    task3 = confirm_file() 
+
+    task1 >> task2 >> task3
 
 source_to_gcs()
