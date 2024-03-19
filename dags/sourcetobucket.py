b7e7168de211ab6bdf51cdbcad1149f9950651ca
Hari-cbe <harisachin1025@gmail.com>
2024-03-19 02:16:39 +0000

    +Created a BQ table and inserted data

--- dags/sourcetobucket.py
+++ dags/sourcetobucket.py
@@ -3,10 +3,14 @@ import pendulum
 import os
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyTableOperator
 
 
 BUCKET = 'used-car-price-analysis'
 GCP_CONN_ID = 'gcp_airflow_conn'
+DATASET_NAME = "used_car_analysis_dataset"
+TABLE_NAME = "used_car_price"
 
 @dag(
     dag_id = "source_to_gcs",
@@ -34,15 +38,26 @@ def source_to_gcs():
         google_cloud_conn_id = GCP_CONN_ID
     )
 
-    @task()
-    def confirm_file():
-        return "The File is present"
+    create_table_BQ = BigQueryCreateEmptyTableOperator(
+        task_id = "create_BQ_table",
+        dataset_id = DATASET_NAME,
+        table_id = TABLE_NAME,
+        gcp_conn_id = GCP_CONN_ID
+    )
 
-    
+    insert_data_bigQuery = GCSToBigQueryOperator(
+        task_id = "insert_data_into_table",
+        bucket = BUCKET,
+        source_objects = [f"data/raw/car_sales.csv"],
+        destination_project_dataset_table=f"{DATASET_NAME}.{TABLE_NAME}",
+        gcp_conn_id = GCP_CONN_ID
+    )
+
     task1 = upload_file 
     task2 = check_file
-    task3 = confirm_file() 
+    task3 = create_table_BQ
+    task4 = insert_data_bigQuery
 
-    task1 >> task2 >> task3
+    task1 >> task2 >> task3 >> task4
 
 source_to_gcs()
