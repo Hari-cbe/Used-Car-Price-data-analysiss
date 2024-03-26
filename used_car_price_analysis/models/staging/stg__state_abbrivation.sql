0a41e7e8b5893ad98846800948b13ed5aca542ef
Hari-cbe <harisachin1025@gmail.com>
2024-03-26 02:34:05 +0000

    fix changes in stg__used_car_price & state_abbrivation column name

--- used_car_price_analysis/models/staging/stg__state_abbrivation.sql
+++ used_car_price_analysis/models/staging/stg__state_abbrivation.sql
@@ -3,11 +3,11 @@ with source as (
     select * from {{ source('used_car_analysis_dataset', 'state_abbrevation') }}
 
 )
-select state, state_abbrevation from (
+select state, state_abbrivation from (
     select
         row_number() over() as rn,
         string_field_1 as state,
-        string_field_2 as state_abbrevation
+        lower(string_field_2) as state_abbrivation
     from source
 ) as b
 where rn > 1
